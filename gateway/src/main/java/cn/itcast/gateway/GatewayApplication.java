b42e8ada04f30b84285833fe1c0dbbe4a41942e9
2021peng <2285070166@qq.com>
2022-11-28 15:58:58 +0800

    gateway

---
+++ gateway/src/main/java/cn/itcast/gateway/GatewayApplication.java
@@ -0,0 +1,21 @@
+package cn.itcast.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @BelongsProject: cloud-demo
+ * @BelongsPackage: cn.itcast.gateway
+ * @Classname: GatewayApplication
+ * @Author: admin
+ * @Date: 2022/11/28 10:32
+ * @Description:
+ */
+
+@SpringBootApplication
+public class GatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
